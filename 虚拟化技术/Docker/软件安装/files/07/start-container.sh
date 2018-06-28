43235918a9d8d880c878911a9ee24e6418b90908
lihongle <lhl_1986@qq.com>
2018-06-28 17:29:03 +0800

    docs:Docker安装Hadoop集群

--- 虚拟化技术/Docker/软件安装/files/07/start-container.sh
+++ 虚拟化技术/Docker/软件安装/files/07/start-container.sh
@@ -10,7 +10,7 @@ echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
-		            -p 9000:9000 \
+		        -p 9000:9000 \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
