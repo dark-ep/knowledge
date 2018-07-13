0ae367a4c85bcfa4a584dc85e5bf0df7de53ebce
lihongle <lhl_1986@qq.com>
2018-07-13 14:45:23 +0800

    docs:Docker安装Zookeeper集群

--- 虚拟化技术/Docker/软件安装/files/05/start-container.sh
+++ 虚拟化技术/Docker/软件安装/files/05/start-container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # start zk1,zk2,zk3 container
-sudo docker rm -f zk1 &> /dev/null
-sudo docker rm -f zk2 &> /dev/null
-sudo docker rm -f zk3 &> /dev/null
+docker-compose -f /home/docker/zk/script/dc-zk.yml rm --force --stop
 echo "start zk1,zk2,zk3 container..."
 docker-compose -f /home/docker/zk/script/dc-zk.yml up -d
