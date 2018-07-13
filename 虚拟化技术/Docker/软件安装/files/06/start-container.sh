85411342fc41edd86efeed2c7ea442cc48cacf0e
lihongle <lhl_1986@qq.com>
2018-07-13 22:25:37 +0800

    docs:Docker安装Mysql

--- 虚拟化技术/Docker/软件安装/files/06/start-container.sh
+++ 虚拟化技术/Docker/软件安装/files/06/start-container.sh
@@ -10,6 +10,6 @@ sudo docker run -d \
                  --hostname mysql \
                  -v /home/docker/mysql/etc/conf.d:/etc/mysql/conf.d \
                  -v /home/docker/mysql/data:/var/lib/mysql \
-                 -v /etc/localtime:/etc/localtime \
+                 -v /etc/localtime:/etc/localtime:ro \
                  -e MYSQL_ROOT_PASSWORD=1qaz2wsx \
                  mysql &> /dev/null
